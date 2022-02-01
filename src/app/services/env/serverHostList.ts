b3da8d71f52bf41b8ea5f9f3f6afe0c8f71f971c
web-gkumar <kgautam431@gmail.com>
2022-02-01 17:42:22 +0530

    multiple chart filter

--- src/app/services/env/serverHostList.ts
+++ src/app/services/env/serverHostList.ts
@@ -1,7 +1,6 @@
-
 export const serverHostList = [
   {
-    "clientEndpoint": "localhost", "serverEndpoint": "http://localhost:8104", "title": "Elabs", "google_map": "false", "menu_type": "Horizontal", "folder": "elabs-client", "temp_name": "temp1", "theme": "", "redirect_url": "http://localhost:4200/#/search",
+    "clientEndpoint": "localhost", "serverEndpoint": "http://localhost:8104", "title": "Elabs", "google_map": "false", "menu_type": "Horizontal", "folder": "elabs-client", "temp_name": "temp1", "theme": "theme1", "redirect_url": "http://localhost:4200/#/search",
     "theme_setting": {
       "header_bg_color": "#fcfcfc",
       "header_txt_color": "",
@@ -19,7 +18,7 @@ export const serverHostList = [
   },
 
   {
-    "clientEndpoint": "drugs.medicea.in", "serverEndpoint": "https://sitservermedicea.medicea.in", "title": "Medicea Technology", "google_map": "false", "menu_type": "Horizontal", "folder": "medicea", "temp_name": "temp1", "theme": "", "redirect_url": "",
+    "clientEndpoint": "drugs.medicea.in", "serverEndpoint": "https://sitservermedicea.medicea.in", "title": "Medicea Technology", "google_map": "false", "menu_type": "Horizontal", "folder": "medicea", "temp_name": "temp1", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "",
       "header_txt_color": "",
@@ -36,7 +35,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "app.medicea.in", "serverEndpoint": "https://servermedicea.medicea.in", "title": "Medicea Technology", "google_map": "false", "menu_type": "Horizontal", "folder": "medicea", "temp_name": "temp1", "theme": "", "redirect_url": "",
+    "clientEndpoint": "app.medicea.in", "serverEndpoint": "https://servermedicea.medicea.in", "title": "Medicea Technology", "google_map": "false", "menu_type": "Horizontal", "folder": "medicea", "temp_name": "temp1", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "",
       "header_txt_color": "",
@@ -53,7 +52,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "lims.inventlands.com", "serverEndpoint": "https://prodinventlandserver.e-labs.ai", "title": "Inventlands", "google_map": "false", "menu_type": "Horizontal", "folder": "inventlands", "temp_name": "temp1", "theme": "", "redirect_url": "",
+    "clientEndpoint": "lims.inventlands.com", "serverEndpoint": "https://prodinventlandserver.e-labs.ai", "title": "Inventlands", "google_map": "false", "menu_type": "Horizontal", "folder": "inventlands", "temp_name": "temp1", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "",
       "header_txt_color": "",
@@ -70,7 +69,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "testlims.inventlands.com", "serverEndpoint": "https://testinventlandserver.e-labs.ai", "title": "Inventlands", "google_map": "false", "menu_type": "Horizontal", "folder": "inventlands", "temp_name": "temp1", "theme": "", "redirect_url": "",
+    "clientEndpoint": "testlims.inventlands.com", "serverEndpoint": "https://testinventlandserver.e-labs.ai", "title": "Inventlands", "google_map": "false", "menu_type": "Horizontal", "folder": "inventlands", "temp_name": "temp1", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "",
       "header_txt_color": "",
@@ -87,7 +86,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "sit-sanskar.e-labs.ai", "serverEndpoint": "https://sitsanskarserver.e-labs.ai", "title": "Sanskar", "google_map": "false", "menu_type": "Horizontal", "folder": "elabs-client", "temp_name": "temp1", "theme": "", "redirect_url": "",
+    "clientEndpoint": "sit-sanskar.e-labs.ai", "serverEndpoint": "https://sitsanskarserver.e-labs.ai", "title": "Sanskar", "google_map": "false", "menu_type": "Horizontal", "folder": "elabs-client", "temp_name": "temp1", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "",
       "header_txt_color": "",
@@ -104,7 +103,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "lims-sanskar.e-labs.ai", "serverEndpoint": "https://prodsanskarserver.e-labs.ai", "title": "Sanskar", "google_map": "false", "menu_type": "Horizontal", "folder": "elabs-client", "temp_name": "temp1", "theme": "", "redirect_url": "",
+    "clientEndpoint": "lims-sanskar.e-labs.ai", "serverEndpoint": "https://prodsanskarserver.e-labs.ai", "title": "Sanskar", "google_map": "false", "menu_type": "Horizontal", "folder": "elabs-client", "temp_name": "temp1", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "",
       "header_txt_color": "",
@@ -121,7 +120,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "itllabs.e-labs.ai", "serverEndpoint": "https://serveritllabs.e-labs.ai", "title": "Itllabs", "google_map": "false", "menu_type": "Horizontal", "folder": "elabs-client", "temp_name": "temp1", "theme": "", "redirect_url": "",
+    "clientEndpoint": "itllabs.e-labs.ai", "serverEndpoint": "https://serveritllabs.e-labs.ai", "title": "Itllabs", "google_map": "false", "menu_type": "Horizontal", "folder": "elabs-client", "temp_name": "temp1", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "",
       "header_txt_color": "",
@@ -138,7 +137,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "hardgoodslabs.e-labs.ai", "serverEndpoint": "https://serverhardgoodlabs.e-labs.ai", "title": "Hardgoodslabs", "google_map": "false", "menu_type": "Horizontal", "folder": "elabs-client", "temp_name": "temp1", "theme": "", "redirect_url": "",
+    "clientEndpoint": "hardgoodslabs.e-labs.ai", "serverEndpoint": "https://serverhardgoodlabs.e-labs.ai", "title": "Hardgoodslabs", "google_map": "false", "menu_type": "Horizontal", "folder": "elabs-client", "temp_name": "temp1", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "",
       "header_txt_color": "",
@@ -155,7 +154,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "demolab.e-labs.ai", "serverEndpoint": "https://serverhardgoodlabs.e-labs.ai", "title": "Demolab", "google_map": "false", "menu_type": "Horizontal", "folder": "elabs-client", "temp_name": "temp1", "theme": "", "redirect_url": "",
+    "clientEndpoint": "demolab.e-labs.ai", "serverEndpoint": "https://serverhardgoodlabs.e-labs.ai", "title": "Demolab", "google_map": "false", "menu_type": "Horizontal", "folder": "elabs-client", "temp_name": "temp1", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "",
       "header_txt_color": "",
@@ -172,7 +171,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "test.ascgroup.in", "serverEndpoint": "https://sitserverascgroup.e-labs.ai", "title": "ASC group", "google_map": "false", "menu_type": "Vertical", "folder": "asc-group", "temp_name": "temp1", "theme": "", "redirect_url": "",
+    "clientEndpoint": "test.ascgroup.in", "serverEndpoint": "https://sitserverascgroup.e-labs.ai", "title": "ASC group", "google_map": "false", "menu_type": "Vertical", "folder": "asc-group", "temp_name": "temp1", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "#fcfcfc",
       "header_txt_color": "",
@@ -189,7 +188,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "erp.ascgroup.in", "serverEndpoint": "https://prodserverascgroup.e-labs.ai", "title": "ASC group", "google_map": "false", "menu_type": "Vertical", "folder": "asc-group", "temp_name": "temp1", "theme": "", "redirect_url": "",
+    "clientEndpoint": "erp.ascgroup.in", "serverEndpoint": "https://prodserverascgroup.e-labs.ai", "title": "ASC group", "google_map": "false", "menu_type": "Vertical", "folder": "asc-group", "temp_name": "temp1", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "#fcfcfc",
       "header_txt_color": "",
@@ -206,7 +205,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "itclabs.e-labs.ai", "serverEndpoint": "https://serveritclabs.e-labs.ai", "title": "Itclabs", "google_map": "false", "menu_type": "Horizontal", "folder": "itclabs", "temp_name": "temp2", "theme": "", "redirect_url": "",
+    "clientEndpoint": "itclabs.e-labs.ai", "serverEndpoint": "https://serveritclabs.e-labs.ai", "title": "Itclabs", "google_map": "false", "menu_type": "Horizontal", "folder": "itclabs", "temp_name": "temp2", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "#665852",
       "header_txt_color": "#ffffff",
@@ -223,7 +222,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "uat-itclabs.e-labs.ai", "serverEndpoint": "https://uatserveritclabs.e-labs.ai", "title": "Itclabs", "google_map": "false", "menu_type": "Horizontal", "folder": "itclabs", "temp_name": "temp2", "theme": "", "redirect_url": "",
+    "clientEndpoint": "uat-itclabs.e-labs.ai", "serverEndpoint": "https://uatserveritclabs.e-labs.ai", "title": "Itclabs", "google_map": "false", "menu_type": "Horizontal", "folder": "itclabs", "temp_name": "temp2", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "#665852",
       "header_txt_color": "#ffffff",
@@ -240,7 +239,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "lims.itclabs.com", "serverEndpoint": "https://serveritclabs.itclabs.com", "title": "Itclabs", "google_map": "false", "menu_type": "Horizontal", "folder": "itclabs", "temp_name": "temp2", "theme": "", "redirect_url": "",
+    "clientEndpoint": "lims.itclabs.com", "serverEndpoint": "https://serveritclabs.itclabs.com", "title": "Itclabs", "google_map": "false", "menu_type": "Horizontal", "folder": "itclabs", "temp_name": "temp2", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "#665852",
       "header_txt_color": "#ffffff",
@@ -293,7 +292,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "lims.qualiteklab.com", "serverEndpoint": "https://serverqualiteklab.qualiteklab.com", "title": "Qualitek Labs", "google_map": "false", "menu_type": "Horizontal", "folder": "qualitek", "temp_name": "temp3", "theme": "", "redirect_url": "",
+    "clientEndpoint": "lims.qualiteklab.com", "serverEndpoint": "https://serverqualiteklab.qualiteklab.com", "title": "Qualitek Labs", "google_map": "false", "menu_type": "Horizontal", "folder": "qualitek", "temp_name": "temp3", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "#fcfcfc",
       "header_txt_color": "",
@@ -327,7 +326,7 @@ export const serverHostList = [
     }
   },
   {
-    "clientEndpoint": "sit-hitech.e-labs.ai", "serverEndpoint": "https://sithitechserver.e-labs.ai", "title": "Hitech Sit", "google_map": "false", "menu_type": "Horizontal", "folder": "hitech", "temp_name": "temp3", "theme": "", "redirect_url": "",
+    "clientEndpoint": "sit-hitech.e-labs.ai", "serverEndpoint": "https://sithitechserver.e-labs.ai", "title": "Hitech Sit", "google_map": "false", "menu_type": "Horizontal", "folder": "hitech", "temp_name": "temp3", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "#fcfcfc",
       "header_txt_color": "",
@@ -345,7 +344,7 @@ export const serverHostList = [
   },
 
   {
-    "clientEndpoint": "lims-hitech.e-labs.ai", "serverEndpoint": "https://prodhitechserver.e-labs.ai", "title": "Hitech Prod", "google_map": "false", "menu_type": "Horizontal", "folder": "hitech", "temp_name": "temp3", "theme": "", "redirect_url": "",
+    "clientEndpoint": "lims-hitech.e-labs.ai", "serverEndpoint": "https://prodhitechserver.e-labs.ai", "title": "Hitech Prod", "google_map": "false", "menu_type": "Horizontal", "folder": "hitech", "temp_name": "temp3", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "#fcfcfc",
       "header_txt_color": "",
@@ -363,7 +362,7 @@ export const serverHostList = [
   },
 
   {
-    "clientEndpoint": "sit-aalbiosciences.e-labs.ai", "serverEndpoint": "https://sitaalbiosciencesserver.e-labs.ai", "title": "Bioscience Sit", "google_map": "false", "menu_type": "Horizontal", "folder": "bioscience", "temp_name": "temp3", "theme": "", "redirect_url": "",
+    "clientEndpoint": "sit-aalbiosciences.e-labs.ai", "serverEndpoint": "https://sitaalbiosciencesserver.e-labs.ai", "title": "Bioscience Sit", "google_map": "false", "menu_type": "Horizontal", "folder": "bioscience", "temp_name": "temp3", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "#fcfcfc",
       "header_txt_color": "",
@@ -381,7 +380,7 @@ export const serverHostList = [
   },
 
   {
-    "clientEndpoint": "lims-aalbiosciences.e-labs.ai", "serverEndpoint": "https://prodaalbiosciencesserver.e-labs.ai", "title": "Bioscience Prod", "google_map": "false", "menu_type": "Horizontal", "folder": "bioscience", "temp_name": "temp3", "theme": "", "redirect_url": "",
+    "clientEndpoint": "lims-aalbiosciences.e-labs.ai", "serverEndpoint": "https://prodaalbiosciencesserver.e-labs.ai", "title": "Bioscience Prod", "google_map": "false", "menu_type": "Horizontal", "folder": "bioscience", "temp_name": "temp3", "theme": "theme1", "redirect_url": "",
     "theme_setting": {
       "header_bg_color": "#fcfcfc",
       "header_txt_color": "",
