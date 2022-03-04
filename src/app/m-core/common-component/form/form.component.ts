665543e5882637012383fdbf6e873dae83a81989
dreamteam012020 <61818195+dreamteam012020@users.noreply.github.com>
2022-03-04 19:34:29 +0530

    Update form.component.ts

--- src/app/m-core/common-component/form/form.component.ts
+++ src/app/m-core/common-component/form/form.component.ts
@@ -4268,8 +4268,9 @@ case 'populate_fields_for_report_for_new_order_flow':
                 value = object;
                 this.templateForm.controls[element.field_name].setValue(value)
               }else if(object == 0){
+                value = object;
                 this.templateForm.controls[element.field_name].setValue(value)
-            });
+             }
             break;            
             case "gmap":
                 if(formValue['longitude']){
