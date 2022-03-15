13095fc0473feaf19a09c74d677e93a1ce2d6724
DELL <neerajquant1221@gmail.com>
2022-03-15 12:12:18 +0530

    create privacy policy template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-import { PrivacyPolicyComponent } from './app-landing/privacy-policy/privacy-policy.component';
 import { VerifyFailedComponent } from './core/error/verify-failed.component';
 
 
