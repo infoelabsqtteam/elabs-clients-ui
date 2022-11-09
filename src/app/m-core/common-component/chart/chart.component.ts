925525ef37c6d0f99c85824153cb5c02510c1579
Swatantra Kumar <rajswatantra9@gmail.com>
2022-11-09 14:31:38 +0530

    check dublicate issue fixed

--- src/app/m-core/common-component/chart/chart.component.ts
+++ src/app/m-core/common-component/chart/chart.component.ts
@@ -215,8 +215,10 @@ testOption = {
   setStaticData(staticData){
     if (staticData) {
       this.staticData = staticData;
-      Object.keys(this.staticData).forEach(key => {        
-        this.copyStaticData[key] = JSON.parse(JSON.stringify(this.staticData[key]));
+      Object.keys(this.staticData).forEach(key => {   
+        if(this.staticData[key]){     
+          this.copyStaticData[key] = JSON.parse(JSON.stringify(this.staticData[key]));
+        }
       }) 
     }
   }
--- src/app/m-core/modals/modals.component.ts
+++ src/app/m-core/modals/modals.component.ts
@@ -83,8 +83,10 @@ export class ModalsComponent implements OnInit,OnDestroy {
   setStaticData(staticData){
     if (staticData) {
       this.staticData = staticData; 
-      Object.keys(this.staticData).forEach(key => {        
-        this.copyStaticData[key] = JSON.parse(JSON.stringify(this.staticData[key]));
+      Object.keys(this.staticData).forEach(key => {    
+        if(this.staticData[key]){    
+          this.copyStaticData[key] = JSON.parse(JSON.stringify(this.staticData[key]));
+        }
       })         
     }
   }
