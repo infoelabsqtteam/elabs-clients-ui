bb029b62e9c853d06a70ecb6539ec465bb6236b5
Swatantra Kumar <rajswatantra9@gmail.com>
2023-05-12 15:06:47 +0530

    on form save downlaod pdf file

--- src/app/m-core/builder/builder.component.ts
+++ src/app/m-core/builder/builder.component.ts
@@ -368,7 +368,7 @@ export class BuilderComponent implements OnInit,OnDestroy {
         this.grid_view_mode="inlineFormView";
       }
       else{
-        if(this.tabs[this.selectTabIndex].grid.grid_view != null && this.tabs[this.selectTabIndex].grid.grid_view != undefined && this.tabs[this.selectTabIndex].grid.grid_view != ''){
+        if(this.tabs[this.selectTabIndex] && this.tabs[this.selectTabIndex].grid && this.tabs[this.selectTabIndex].grid.grid_view != null && this.tabs[this.selectTabIndex].grid.grid_view != undefined && this.tabs[this.selectTabIndex].grid.grid_view != ''){
           this.grid_view_mode=this.tabs[this.selectTabIndex].grid.grid_view; 
         }
         else if(this.tabs[this.selectTabIndex].chart_list != null && this.tabs[this.selectTabIndex].chart_list != undefined && this.tabs[this.selectTabIndex].chart_list != ''){
