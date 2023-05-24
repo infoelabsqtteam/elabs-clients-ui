f4bc00675c3739710aff647e5a34000c07c3b15a
SoniaIT <soniaarora0509@gmail.com>
2023-05-24 18:32:55 +0530

    Data Size Change In Charts Page

--- src/app/m-core/common-component/mongodb-chart/mongodb-chart.component.ts
+++ src/app/m-core/common-component/mongodb-chart/mongodb-chart.component.ts
@@ -21,7 +21,7 @@ export class MongodbChartComponent implements OnInit,AfterViewInit {
   accessToken:string="";
   @Input() showMongoChart:boolean;
   pageNumber:any=1;
-  itemNumOfGrid: any = 25;
+  itemNumOfGrid: any = 26;
   gridDataSubscription:Subscription;
   darkTheme:any={};
 
