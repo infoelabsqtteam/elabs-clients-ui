bab6c1af9d25183bdbca4b725c40e5c1caa23916
Swatantra Kumar <rajswatantra9@gmail.com>
2023-07-31 19:03:19 +0530

    get dashbord and dashbard chart from chart list

--- src/app/m-core/common-component/dashboard-chart/dashboard-chart.component.ts
+++ src/app/m-core/common-component/dashboard-chart/dashboard-chart.component.ts
@@ -11,19 +11,15 @@ import { StorageService, CommonFunctionService, ApiService, ChartService, ModelS
 export class DashboardChartComponent implements OnInit,AfterViewInit,OnDestroy {
 
   chartIdList:any = [];
-  dashboardChartIdList:any = [];
-  createdDashbordList:any=[];
+  dashboardIdList:any = [];
+  dashbord:any={};
   accessToken:string="";
   @Input() showDashboardMongoChart:boolean;
   pageNumber:any=1;
-  itemNumOfGrid: any = 6;
+  itemNumOfGrid: any = 25;
   gridDataSubscription:Subscription;
+  dashbordSubscription:Subscription;
   darkTheme:any={};
-  total;
-  noOfItems:any = [
-    6,9,12,15,18,21,24
-  ]
-  charts: any;
 
   constructor(
     private dataShareService:DataShareService,
@@ -33,7 +29,12 @@ export class DashboardChartComponent implements OnInit,AfterViewInit,OnDestroy {
     private modelService:ModelService,
     private chartService:ChartService
   ) {
-    this.accessToken = this.storageService.GetIdToken();      
+    this.accessToken = this.storageService.GetIdToken();     
+    this.dashbordSubscription = this.dataShareService.mongoDashbordList.subscribe(data =>{
+      this.dashboardIdList = data.data;
+      this.populateDashbord(this.dashboardIdList);
+      console.log(data.data);
+    }) 
     
       //  this.getPage(1); 
    }
@@ -46,7 +47,25 @@ export class DashboardChartComponent implements OnInit,AfterViewInit,OnDestroy {
         data: data,
         path: null
       }
-      this.apiService.getMongoDashletMster(getFilterData)
+      return getFilterData;      
+  }
+  populateDashbord(list){
+    if(list && list.length > 0){
+      this.dashbord = list[0];
+      if(this.dashbord && this.dashbord.chartList && this.dashbord.chartList.length > 0){
+        let chartList = this.dashbord.chartList;
+        let chartIdList = [];
+        chartList.forEach(chart => {
+          chartIdList.push(chart._id);
+        });
+        if(chartIdList && chartIdList.length > 0){
+          let criteriaList = [];
+          let cr = "_id;in;"+chartIdList+";STATIC";
+          criteriaList.push(cr);
+          this.getPage(1,criteriaList);
+        }
+      }
+    }
   }
 
   ngOnInit() {
@@ -61,95 +80,35 @@ export class DashboardChartComponent implements OnInit,AfterViewInit,OnDestroy {
     if(this.gridDataSubscription){
       this.gridDataSubscription.unsubscribe();
     }
+    if(this.dashbordSubscription){
+      this.dashbordSubscription.unsubscribe();
+    }
   }
   ngOnChanges(changes: SimpleChanges): void {
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
     //Add '${implements OnChanges}' to the class.
     if(this.showDashboardMongoChart){
       this.subscribe();
-      this.getPage(1);
-      setTimeout(() => {
-        this.populateDashboardChart();
-      }, 100);
+      this.getDashbord(1);
+      // setTimeout(() => {
+      //   this.populateDashboardChart();
+      // }, 100);
     }
     
   }
   subscribe(){
     this.gridDataSubscription = this.dataShareService.mongoDbChartList.subscribe(data =>{
-      this.total = data.data_size; 
       const chartData = data.data;
       if(chartData && chartData.length > 0){
         this.chartIdList = chartData;
         setTimeout(() => {
-          this.populateDashboardChart();
+          
         }, 100);
       }
     })
   }
-  populateDashboardChart(){
-    if(this.accessToken != "" && this.accessToken != null){      
-      let height = '800px';
-      if(this.chartIdList && this.chartIdList.length > 0){        
-        this.createdDashbordList = [];
-        for (let i = 0; i < this.chartIdList.length; i++) {
-         // const url = this.chartIdList[i].chartUrl;
-         let url = 'https://charts.mongodb.com/charts-nonproduction-cgurq';
-          if(url && url != ''){
-            const sdk = new ChartsEmbedSDK({
-              baseUrl: url, // Optional: ~REPLACE~ with the Base URL from your Embed Chart dialog
-              getUserToken: () => this.accessToken
-            });
-            let chart = this.chartIdList[i];
-            // const id = chart.chartId;
-            let id ="b2f745ea-8d03-4337-996e-e53e986058d0";
-            const idRef = document.getElementById(id);
-            if(chart && chart.height && chart.height != ""){
-              height = chart.height;
-            }
-         //   if(idRef){
-              let cretedChart = sdk.createDashboard({
-                dashboardId: id, // Optional: ~REPLACE~ with the Chart ID from your Embed Chart dialog
-                height: height,
-                background:'black',
-                showAttribution:false,
-                showTitleAndDesc:true                
-              });
-              this.createdDashbordList[id] = cretedChart;
-              let test = cretedChart.getImage({encoding:'base64'});
-              console.log(test);
-              cretedChart
-              .render(idRef)
-              .catch(() =>
-              console.log('Chart failed to initialise')
-              // window.alert('Chart failed to initialise')
-              );
-          //  }
-          }
-        }        
-      }
-    }
-  }
 
 
-getChartListFromDashboardId(){
-  const data = {
-    id: "b2f745ea-8d03-4337-996e-e53e986058d0",
-    url: "https://charts.mongodb.com/charts-nonproduction-cgurq",
-  };
-
-  const sdk = new ChartsEmbedSDK({
-    baseUrl: data.url, // Optional: ~REPLACE~ with the Base URL from your Embed Chart dialog
-    getUserToken: () => this.accessToken
-  });
-
-  const id = data.id;
-  const idRef = document.getElementById(id);
-
-  const cretedChart = sdk.createDashboard({
-    dashboardId: data.id, // Optional: ~REPLACE~ with the Chart ID from your Embed Chart dialog
-  }).getAllCharts();
-   console.log(cretedChart);
-}
 
   filterModel(data:any,filter:any){
     let object = {
@@ -159,31 +118,6 @@ getChartListFromDashboardId(){
     }
     this.modelService.open('chart-filter',object);
   }
-  download(){
-    let chartId = 'b2f745ea-8d03-4337-996e-e53e986058d0';
-    let chart = this.createdDashbordList[chartId];
-    // if(allCharts && allCharts.length > 0){
-    //   allCharts.forEach(chart => {
-    //     chart.getData().then(data => {
-    //       console.log(data);
-    //     })
-    //   });
-    //}
-    // chart.getImage({ encoding: 'base64'}).then((chartdata: string) => { 
-    //   console.log(chartdata);   
-      
-    // }); 
-    //this.chartService.getDownloadData(chart,object);
-  }  
-  changeTheme(object,value){
-    let chartId = object.chartId;
-    let chart = this.createdDashbordList[chartId];
-    if(value){
-      chart.setTheme("dark");
-    }else{
-      chart.setTheme("light");
-    }
-  }
 
   selectNoOfItem(){
     this.getPage(1);
@@ -191,6 +125,18 @@ getChartListFromDashboardId(){
 
 
   getPage(page: number,criteria?:any) {
+    let Criteria:any = [];
+    let cr=["status;eq;Active;STATIC","package_name;eq;mongodb_chart;STATIC"];
+    Criteria= cr;
+    if(criteria && criteria.length > 0){
+      criteria.forEach(data => {
+        Criteria.push(data);
+      });     
+    }
+    this.pageNumber = page;
+    this.apiService.getMongoDashletMster(this.getMongoChartList(Criteria));
+  }
+  getDashbord(page: number,criteria?:any) {
     let Criteria:any = [];
     let cr=["status;eq;Active;STATIC","package_name;eq;mongodb_dashboard;STATIC"];
     Criteria= cr;
@@ -200,7 +146,7 @@ getChartListFromDashboardId(){
       });     
     }
     this.pageNumber = page;
-    this.getMongoChartList(Criteria);
+    this.apiService.getMongoDashbord(this.getMongoChartList(Criteria));
   }
 
 }
