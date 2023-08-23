337123b14190afba89acee9844f2b28f0d3d207c
Swatantra Kumar <rajswatantra9@gmail.com>
2023-08-23 19:50:35 +0530

    add new case for subsequent calculation

--- src/app/m-core/common-component/form/form.component.ts
+++ src/app/m-core/common-component/form/form.component.ts
@@ -2421,6 +2421,10 @@ export class FormComponent implements OnInit, OnDestroy, OnChanges, AfterViewIni
           calFormValue = this.limsCalculationsService.calculate_quotation(tamplateFormValue,"standard", field);
           this.updateDataOnFormField(calFormValue);
           break;
+        case 'calculate_quotation_with_subsequent':          
+          calFormValue = this.limsCalculationsService.calculate_quotation_with_subsequent(tamplateFormValue,"standard", field);
+          this.updateDataOnFormField(calFormValue);
+          break;
         case 'calculate_automotive_quotation':          
           calFormValue = this.limsCalculationsService.calculate_quotation(tamplateFormValue,"automotive" ,field);
           this.updateDataOnFormField(calFormValue);
