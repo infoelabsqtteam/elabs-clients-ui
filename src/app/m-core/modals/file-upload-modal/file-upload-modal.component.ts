fe29f5fc80c95e05ee015b5a3e6dfde48107a9a1
vikash <vksh6623@gmail.com>
2023-09-09 13:02:30 +0530

    new field support file-for-s3

--- src/app/m-core/modals/file-upload-modal/file-upload-modal.component.ts
+++ src/app/m-core/modals/file-upload-modal/file-upload-modal.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { ModalDirective } from 'angular-bootstrap-md';
 import { CommonFunctionService, ModelService, NotificationService, StorageService} from '@core/web-core';
+import * as S3 from 'aws-sdk/clients/s3';
+import { DocApiService } from '@core/web-core';
+import { config } from '../../document/config.modal';
 
 
 @Component({
@@ -19,13 +22,22 @@ export class FileUploadModalComponent implements OnInit {
   field:any={};
   fileSize:any=0;
   fileSizeHints:any = '';
+  uploadProgressValue = 0;
+  currentFileIndex:any=-1;
+  tableFields:any[] = [];
+  s3bucket:any=''
+  s3Key:any=''
+  fileForS3:boolean = false;
+  uploadStart:boolean=true;
+  uploadDataForS3: any = [];
 
   constructor(
     private modalService: ModelService, 
     private el: ElementRef,
     private storageService: StorageService,
     private notificationService:NotificationService,
-    private commonfunctionService:CommonFunctionService
+    private commonfunctionService:CommonFunctionService,
+    private docApiService:DocApiService,
     ) { }
 
   ngOnInit(): void {
@@ -128,7 +140,7 @@ export class FileUploadModalComponent implements OnInit {
       fileData: e.target.result.split(',')[1],
       fileName: rxFile.name,
       fileExtn:  rxFile.name.split(".").pop(),
-      // size: rxFile.size,
+      size: rxFile.size,
       innerBucketPath: rxFile.name,
       log: this.storageService.getUserLog()
     });
@@ -192,13 +204,131 @@ export class FileUploadModalComponent implements OnInit {
       this.fileSize = 0;
       this.fileSizeHints = '';
     }
+    this.tableFields = object.tableFields;
+    this.getBucketAndS3Key(object);
+    if(this.field.type == 'file_for_s3'){
+      this.fileForS3 = true;
+    }
+    this.uploadStart = true;
+    this.uploadDataForS3 = [];
     this.docUploadModal.show();
   }
+
+  getBucketAndS3Key(object) {
+
+    //if default value is defined in field
+    this.s3bucket = this.field && this.field.extraInfo != undefined && this.field.extraInfo != null ?  this.field.extraInfo.defaultBucket : '';
+    this.s3Key = this.field && this.field.extraInfo != undefined && this.field.extraInfo != null ?  this.field.extraInfo.defaultS3Key : '';
+
+    this.s3bucket = object.defaultBucket != undefined || object.defaultBucket != null ? object.defaultBucket : '';
+    this.s3Key = object.defaultS3Key != undefined || object.defaultS3Key != null ? object.defaultS3Key : '';
+
+    //if we added defaultBucket & defaultS3Key hidden fields in form
+    if(this.s3bucket == '' || this.s3bucket == undefined){
+      this.findDefaultBucket();
+    }
+    if(this.s3Key == '' || this.s3Key == undefined){
+      this.findDefaultKey();
+    }
+  }
+
   cancel() {
     this.docUploadModal.hide();
     this.fileSize = 0;
     this.fileSizeHints = '';
     //this.fileUploadResponce.emit(this.uploadData);
   }
+
+  uploadFilesSimulatorForS3(){
+    this.uploadByS3(); 
+  }
+
+  uploadByS3(index?){
+    this.uploadStart = false;
+
+    // const file = this.files[0];
+    if(index == null || index == undefined) index=0;
+    let file = this.files[index];
+    // this.files.forEach((file,i)=>{
+      this.currentFileIndex = index;
+      if(this.s3bucket != '' && this.s3bucket != undefined){
+        const bucket = new S3(config);	        
+        const params = {
+          Bucket: this.s3bucket,
+          Key: this.s3Key+'/'+file.name,
+          Body: file,
+          ACL: 'private',
+          ContentType: file.type
+        };
+
+        bucket.upload(params).on('httpUploadProgress', (evt) => {        
+          this.uploadProgressValue = Math.round(100.0 * (evt.loaded / evt.total));
+          //console.log(this.uploadProgressValue + ' %' + evt.loaded + ' of ' + evt.total + ' Bytes');
+        }).send( (err, data) => {
+          if (err) {
+            this.notificationService.notify("success","There was an error uploading your file: "+ err);
+            //console.log('There was an error uploading your file: ', err);
+            return false;
+          }	
+          // ++this.currentFileIndex;
+
+          this.uploadProgressValue = 0;		
+          let s3Object = {
+            "bucket":data.Bucket,
+            "rollName":file.name,
+            "key": data.Key,
+            "capKey":data.Key.toUpperCase(),
+            "fileExt":file.name.substring(file.name.lastIndexOf('.'))
+          }
+          this.uploadDataForS3.push(s3Object);
+          ++index;
+          if(this.files[index] != null || this.files[index] != undefined){
+            this.uploadByS3(index)
+          }else{
+            this.files = [];
+            this.uploadData = [];
+            this.fileUploadResponce.emit(this.uploadDataForS3);
+            this.cancel();
+          }
+          // newObj.key = key;
+          // newObj.capKey = key.toUpperCase();
+          // newObj.bucket = data.Bucket;
+          // this.files[this.currentFileIndex].upload = true;
+          // this.uploadProgressValue = 0;
+          // this.docApiService.SaveUploadFile(newObj);
+          // this.uploadFilesData = [];					
+          // return true;
+        });
+      }else{
+        this.notificationService.notify('bg-danger','Bucket Name is Required !!!');
+      }
+    // })
+    // this.uploadData = [];
+    // this.currentFileIndex = -1;
+    // this.files = [];
+    // this.cancel();
+	}
+
+  findDefaultBucket(){
+    if(this.s3bucket == '' || this.s3bucket == undefined){
+      if(this.tableFields.length > 0){
+        let defaultBucketTableFields = this.tableFields.filter(x => x.field_name == 'defaultBucket');
+        if(defaultBucketTableFields.length >0){
+          this.s3bucket = defaultBucketTableFields[0].defaultValue;
+        }
+      }
+    }
+  }
+
+  findDefaultKey(){
+    if(this.s3Key == '' || this.s3Key == undefined){
+      if(this.tableFields.length > 0){
+        let defaultKeyTableFields = this.tableFields.filter(x => x.field_name == 'defaultS3Key');
+        if(defaultKeyTableFields.length >0){
+          this.s3Key = defaultKeyTableFields[0].defaultValue;
+        }
+      } 
+    }
+}
 
 }
