7c0150fe30819abe82a1e9d82204161eb74b8ec5
kwebdev11 <kunalwebdeveloper11@gmail.com>
2023-11-16 16:53:52 +0530

    updated library version of angular 14

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  serverhost: "https://prodinventlandserver.e-labs.ai/rest/",
+  // serverhost: "https://prodinventlandserver.e-labs.ai/rest/",
+  serverhost: "https://uatserveritclabs.e-labs.ai",
   appId: "ELABS",
   appName: "elabsprod"
 };
