9b0bc2d4bd493620ba8d200830c1f10328118b86
Swatantra Kumar <rajswatantra9@gmail.com>
2023-12-07 13:56:57 +0530

    remove unused file import

--- src/app/m-core/modals/form-model.module.ts
+++ src/app/m-core/modals/form-model.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormModalComponent } from './form-modal/form-modal.component';
 import { FranchiseComponent } from './franchise/franchise.component';
-import { AllPackageModule } from '../all-package/all-package.module';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { ChartModalComponent } from './chart-modal/chart-modal.component';
 import { CommonFormComponentModule } from '../common-form-component/common-form-component.module';
 
 const models = [
