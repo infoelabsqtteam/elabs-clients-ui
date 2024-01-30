2fcc1de7a7ccfc43335dc8245d504b1e8fe29474
Swatantra Kumar <rajswatantra9@gmail.com>
2024-01-30 18:44:24 +0530

    update packages in module

--- src/app/m-core/notification/notification.module.ts
+++ src/app/m-core/notification/notification.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotificationSettingComponent } from './notification-setting/notification-setting.component';
 import { NotificationListComponent } from './notification-list/notification-list.component';
+import { AngularMaterialModule } from '../angular-material-module/angular-material.module';
+import { AllPackageModule } from '../all-package/all-package.module';
 
 let components = [
   NotificationSettingComponent,
@@ -9,7 +11,9 @@ let components = [
 ]
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AngularMaterialModule,
+    AllPackageModule
   ],
   declarations: components,
   exports : components
