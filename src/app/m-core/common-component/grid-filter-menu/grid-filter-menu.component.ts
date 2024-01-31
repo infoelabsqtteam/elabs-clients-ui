871a6163ee04321e7fed2983eebe736265633131
kalki39 <kalkiram39@gmail.com>
2024-01-31 13:20:47 +0530

    save_button_disabled

--- src/app/m-core/common-component/grid-filter-menu/grid-filter-menu.component.ts
+++ src/app/m-core/common-component/grid-filter-menu/grid-filter-menu.component.ts
@@ -1,15 +1,16 @@
-import { Component, Input , OnInit} from "@angular/core";
-import { StorageService, CommonFunctionService, PermissionService, DataShareService, ApiService, NotificationService, EnvService, MenuOrModuleCommonService, ApiCallService, UserPrefrenceService} from '@core/web-core';
+import { Component, Input, OnInit } from "@angular/core";
+import {StorageService,CommonFunctionService,PermissionService,DataShareService,ApiService,NotificationService,EnvService,MenuOrModuleCommonService,ApiCallService,UserPrefrenceService,} from "@core/web-core";
 
 @Component({
-  selector: 'app-grid-filter-menu',
-  templateUrl: './grid-filter-menu.component.html',
-  styleUrls: ['./grid-filter-menu.component.css'],
+  selector: "app-grid-filter-menu",
+  templateUrl: "./grid-filter-menu.component.html",
+  styleUrls: ["./grid-filter-menu.component.css"],
 })
 export class GridFilterMenuComponent implements OnInit{
   @Input() columns: any;
   @Input() form: any;
   @Input() formTable: any;
+  allCheck = false;
 
   constructor(
     private storageService: StorageService,
@@ -17,12 +18,9 @@ export class GridFilterMenuComponent implements OnInit{
     private dataShareService: DataShareService,
     private commonFunctionService: CommonFunctionService,
     private userPrefrenceService: UserPrefrenceService,
-    private notificationService: NotificationService,
+    private notificationService: NotificationService
   ) {}
-  showHide(){
-  }
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   updateColumnList() {
     if (this.columns) {
@@ -30,35 +28,39 @@ export class GridFilterMenuComponent implements OnInit{
     }
   }
 
-  saveColumns(columns: any[]){
-    const unCheckedFields=columns.filter((col:any)=>col.display==false);
-    if(columns && columns.length == unCheckedFields.length){
-      this.notificationService.notify('bg-warning', 'Atleast select one field');
-      return;
+  checkColumnList(columns) {
+    const unCheckedFields = columns.filter((col: any) => col.display == false);
+    if (columns && columns.length == unCheckedFields.length) {
+      this.allCheck = true;
+    } else {
+      this.allCheck = false;
     }
-    let data={
-      columns,
-      form:this.form,
-      formTable:this.formTable
-    }
-    let formId="";
-    let formFieldName="";
-    if(this.form && this.formTable){
-      formId=this.form._id;
-      formFieldName=this.formTable.field_name;
-    }
-    this.updateUserPreference(data,"preference",formId,formFieldName,columns)
   }
 
-  async updateUserPreference(data,field,formId,formFieldName,columns){
+  saveColumns(columns: any[]) {
+    if (!this.allCheck) {
+      let data = {
+        columns,
+        form:this.form,
+        formTable:this.formTable
+      };
+      let formId = "";
+      let formFieldName = "";
+      if (this.form && this.formTable) {
+        formId = this.form._id;
+        formFieldName = this.formTable.field_name;
+      }
+      this.updateUserPreference( data, "preference",formId,formFieldName,columns);
+    }
+  }
+
+  async updateUserPreference(data, field, formId, formFieldName, columns) {
     let response = await this.userPrefrenceService.updateUserPreference(data,field);
     if (response?.success) {
       this.userPrefrenceService.addHideKeyInExistingTab(columns,formId,formFieldName);
-      this.notificationService.notify('bg-success', 'Column Field updated successfully!');
+      this.notificationService.notify("bg-success","Column Field updated successfully!");
     } else {
-      this.notificationService.notify('bg-warning', 'Failed to save data.');
+      this.notificationService.notify("bg-warning", "Failed to save data.");
     }
   }
 }
-
-
