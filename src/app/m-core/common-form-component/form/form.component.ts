4ae8dbb7aea8ef44a988eb4993779d08756a4ab8
web-gkumar <kgautam431@gmail.com>
2024-01-31 10:40:40 +0530

    grid selection data fixing

--- src/app/m-core/common-form-component/form/form.component.ts
+++ src/app/m-core/common-form-component/form/form.component.ts
@@ -2356,8 +2356,13 @@ export class FormComponent implements OnInit, OnDestroy, OnChanges, AfterViewIni
         payloads.push(this.apiCallService.getPaylodWithCriteria(this.curTreeViewField.onchange_function_param,'',[],this.getFormValue(true)));
         this.callStaticData(payloads);
       }
-    }    
-    this.modifyCustmizedValue(fieldName);
+    }
+    if(this.custmizedFormValue[fieldName] && this.custmizedFormValue[fieldName].length > 0) {
+      this.modifyCustmizedValue(fieldName);
+    } else {
+      this.custmizedFormValue[fieldName] = [];
+      this.modifyCustmizedFormValue = [];
+    }
     this.curTreeViewField = {};
     this.currentTreeViewFieldParent = {};
   }
