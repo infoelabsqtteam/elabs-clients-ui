b1b6b9fdebd72c398cf6fdb05752035a63add316
web-gkumar <kgautam431@gmail.com>
2024-02-15 11:02:50 +0530

    bulk uploading fixing

--- src/app/m-core/modals/grid-selection-modal/grid-selection-modal.component.ts
+++ src/app/m-core/modals/grid-selection-modal/grid-selection-modal.component.ts
@@ -803,9 +803,7 @@ export class GridSelectionModalComponent implements OnInit {
         if(data.selected || !this.grid_row_selection){
           for (let j = 0; j < this.editableGridColumns.length; j++) {
             const column = this.editableGridColumns[j];
-            if(responce[column.field_name] != undefined) {
-              data[column.field_name] = responce[column.field_name];
-            }
+            // data[column.field_name] = responce[column.field_name];
             this.checkIfService.checkDisableInRow(this.editableGridColumns,data);
             if(data && !data[column.field_name+"_disabled"] && responce[column.field_name] && column.display){
               data[column.field_name] = responce[column.field_name];
