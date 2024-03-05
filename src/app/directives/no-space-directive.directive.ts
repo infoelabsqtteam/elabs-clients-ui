819f2b98b30d73bd344bdd89152f11e0f1d0d8e7
DEIVA SIVAM <115927155+asdeiva@users.noreply.github.com>
2024-03-05 16:44:41 +0530

    directive updated

--- src/app/directives/no-space-directive.directive.ts
+++ src/app/directives/no-space-directive.directive.ts
@@ -1,25 +1,20 @@
-import { Directive, Self, Output, EventEmitter } from '@angular/core';
+import { Directive, Self, Input } from '@angular/core';
 import { MatSelect } from '@angular/material/select';
 
 @Directive({
-  selector: 'mat-select',
+  selector: '[disable-space-select]',
 })
 export class NoSpaceDirective {
-  @Output('spacekeydown') spacekeydown: EventEmitter<any> =
-    new EventEmitter<any>();
 
   constructor(@Self() private select: MatSelect) {
     this.select._handleKeydown = (event: KeyboardEvent) => {
       if (event.code == 'Space') {
-        const active = this.select.panelOpen
-          ? this.select.options.filter((x) => x.active)[0] || null
-          : null;
-        this.spacekeydown.emit(active ? active.value : null);
+        return;
       } else {
         if (!this.select.disabled) {
           this.select.panelOpen
-            ? (this.select as any)._handleOpenKeydown(event)
-            : (this.select as any)._handleClosedKeydown(event);
+          ? (this.select as any)._handleOpenKeydown(event)
+          : (this.select as any)._handleClosedKeydown(event);
         }
       }
     };
