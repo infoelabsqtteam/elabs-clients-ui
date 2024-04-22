f5a89b7c82dda22637ec5265997189036adf96d6
DEIVA SIVAM <115927155+asdeiva@users.noreply.github.com>
2024-04-22 19:26:19 +0530

    fn renamed

--- src/app/core/setting-menu/setting-menu.component.ts
+++ src/app/core/setting-menu/setting-menu.component.ts
@@ -850,7 +850,7 @@ export class SettingMenuComponent implements OnInit, OnDestroy {
 
     refreshEndpoint(){
         this.cookiesService.deleteCookieByName(this.envService.getHostName('hostname'))
-        this.dataShareService.getServerEndPoint.next(true);
+        this.dataShareService.getServerEndPoint();
     }
 
 }
