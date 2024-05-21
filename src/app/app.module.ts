ca7194686c025c2671fe8654a152672f37e22950
Swatantra Kumar <rajswatantra9@gmail.com>
2024-05-21 17:44:54 +0530

    remvoe filter pipe form app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { DatePipe,CurrencyPipe,TitleCasePipe,DecimalPipe,HashLocationStrategy, L
 import { HttpClientModule,HttpClientXsrfModule,HttpClient } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { FilterPipe } from './pipes/filter.pipe';
 import { AuthModule } from './auth/auth.module';
 import { McoreModule } from './m-core/m-core.module';
 import { AppLandingModule } from './app-landing/app-landing.module';
@@ -37,8 +36,7 @@ import { AppRoutingModule } from './app-routing.module';
     TitleCasePipe, 
     DecimalPipe,
     Location , 
-    {provide: LocationStrategy, useClass: PathLocationStrategy},
-    FilterPipe
+    {provide: LocationStrategy, useClass: PathLocationStrategy}
   ],
 })
 export class AppModule { }
